7705daa380d6f5dca3e55cecf3431668d6405109
ostapViniasvkyi <viniavskyi@ucu.edu.ua>
2020-05-08 20:01:11 +0300

    Update 3d model and VAE training procedures

---
+++ brats_competition/model_training/segmentation/train_vae.py
@@ -0,0 +1,32 @@
+import yaml
+import torch
+import numpy as np
+import os
+import random
+
+from brats_competition.model_training.common.trainer import Trainer
+from brats_competition.model_training.common.datasets import Brats3dDataset
+from brats_competition.model_training.common.augmentations import get_transforms
+
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+with open(os.path.join(os.path.dirname(__file__), 'config', 'unet3dvae.yaml')) as config_file:
+    config = yaml.full_load(config_file)
+
+train_transform = get_transforms(config['train']['transform'])
+val_transform = get_transforms(config['val']['transform'])
+
+train_ds = Brats3dDataset(config['train']['path'], transform=train_transform)
+val_ds = Brats3dDataset(config['val']['path'], transform=val_transform)
+
+train_dl = torch.utils.data.DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True, num_workers=4,
+                                       pin_memory=True)
+val_dl = torch.utils.data.DataLoader(val_ds, batch_size=config['batch_size'], shuffle=True, num_workers=4,
+                                     pin_memory=True)
+
+trainer = Trainer(config, train_dl, val_dl)
+trainer.train()
